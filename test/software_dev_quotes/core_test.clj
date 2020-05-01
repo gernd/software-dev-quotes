a36b2e081d78d8b3970d47667bb8623e4a8a1eb1
Gernot Pointner <gernot.pointner@jambit.com>
2020-05-01 11:51:55 +0200

    Fix tests

---
+++ test/software_dev_quotes/core_test.clj
@@ -0,0 +1,7 @@
+(ns software-dev-quotes.core-test
+  (:require [clojure.test :refer :all]
+            [software-dev-quotes.core-test :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
