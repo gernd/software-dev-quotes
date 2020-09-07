429458bd79f79978ceecb9ff8f6b91d1c319e7a4
Gernot Pointner <gernot.pointner@googlemail.com>
2020-09-07 07:32:31 +0200

    Add random quotes, remove dummy content

--- src/software_dev_quotes/core.clj
+++ src/software_dev_quotes/core.clj
@@ -2,30 +2,39 @@
   (:use ring.adapter.jetty)
   (:use hiccup.core)
   (:use ring.middleware.content-type)
+  (:use clojure.tools.trace)
   (:require [environ.core :refer [env]])
   (:gen-class))
 
 (def quotes [{:quote "The real problem is that programmers have spent far too much time worrying about efficiency
              in the wrong places and at the wrong times; premature optimization is the root of all evil
-             (or at least most of it) in programming." :from "Donald Knuth"}])
+             (or at least most of it) in programming." :from "Donald Knuth"}
+             {:quote "Any fool can write code that a computer can understand.
+              Good programmers write code that humans can understand." :from "Martin Fowler"}
+             ])
+
+(defn get-random-quote []
+  "Returns a random quote from the available quotes"
+  (nth quotes (rand-int (count quotes))))
 
 (defn my-handler
   [request]
-  {:status  200
-   :headers {"Content-Type" "text/html; charset=UTF-8"}
-   :body    (html [:html
-                   [:head
-                    [:title "Software Development quotes"]
-                    [:meta {:charset "utf-8"}]
-                    [:link {:href "https://fonts.googleapis.com/css?family=Press+Start+2P" :rel "stylesheet"}]
-                    [:link {:href "https://unpkg.com/nes.css@2.3.0/css/nes.min.css" :rel "stylesheet"}]]
-                   [:body [:p "Hello folks from the software dev quotes app"]
-                    [:div.nes-container.with-title.is-centered
-                     [:p.title "Random quote"]
-                     [:p.nes-balloon.from-left "This is a very clever quote. - someone"]
-                     [:p [:i.nes-icon.is-small.like]]
-                     [:p "Good morning. Thou hast had a good night's sleep, I hope."]]
-                    [:a {:href "https://www.github.com"} [:i.nes-icon.github.is-small] " Contribute on github"]]])})
+  (let [random-quote (get-random-quote)]
+    {:status  200
+     :headers {"Content-Type" "text/html; charset=UTF-8"}
+     :body    (html [:html
+                     [:head
+                      [:title "Software Development quotes"]
+                      [:meta {:charset "utf-8"}]
+                      [:link {:href "https://fonts.googleapis.com/css?family=Press+Start+2P" :rel "stylesheet"}]
+                      [:link {:href "https://unpkg.com/nes.css@2.3.0/css/nes.min.css" :rel "stylesheet"}]]
+                     [:body [:h1 "Hello folks from the software dev quotes app"]
+                      [:div.nes-container.with-title.is-centered
+                       [:p.title "Random quote"]
+                       [:p.nes-balloon.from-left (str (:quote random-quote) " - " (:from random-quote))]
+                       ]
+                      [:a {:href "https://www.github.com"} [:i.nes-icon.github.is-small] " Contribute on github"]]])}
+    ))
 
 (defn -main
   "Fires up the Ring app"
