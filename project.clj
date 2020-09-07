7ab53a70370c2e7befa386ffb6f35d46d7e4b2fe
Gernot Pointner <gernot.pointner@googlemail.com>
2020-09-07 07:43:02 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring "1.8.0"]
-                 [environ "1.1.0"]
+                 [ring "1.8.1"]
+                 [environ "1.2.0"]
                  [hiccup "1.0.5"]
                  [org.clojure/tools.trace "0.7.10"]
                  ]
