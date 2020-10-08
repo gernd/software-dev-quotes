9474351b55372ceb72388e48a9046c4a246b7ff5
Gernot Pointner <gernot.pointner@googlemail.com>
2020-10-08 19:57:06 +0200

    Update ring version to 1.8.2

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring "1.8.1"]
+                 [ring "1.8.2"]
                  [environ "1.2.0"]
                  [hiccup "1.0.5"]
                  [org.clojure/tools.trace "0.7.10"]
